8659692519627441deb22ee57a659b2428cf2838
Matheus <matheusuniversidade@hotmail.com>
2024-08-16 19:12:12 -0300

    Created repository

---
+++ src/main_page.js
@@ -0,0 +1,16 @@
+import './main_page.css';
+
+// js file specific to build the main page and its architeture
+
+function loadMainPage() {
+    <div id='container'>
+        <nav></nav>
+        <header>
+            <button id='section-creator'>
+                + Create Section
+            </button>
+            <button id='switch-to-uml'></button>
+        </header>
+        
+    </div>
+}
